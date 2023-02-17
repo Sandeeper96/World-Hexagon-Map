2eefa3bde95d80b8f1d9fafeca849574d9e92b5a
Sandeep kumar <sandeeper96@gmail.com>
2023-02-17 18:20:58 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import WorldHexagonMap from './components/WorldHexagonMap/WorldHexagonMap';
+import { BubbleChart } from './components/BubbleChart';
+import { WORLDHEXAGONMAP } from './components/WORLDHEXAGONMAP';
+import Overlay from './components/overlay/Overlay';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <WorldHexagonMap/>
+    <Overlay><WorldHexagonMap/></Overlay>
+    <BubbleChart/>
+      
     </div>
   );
 }
